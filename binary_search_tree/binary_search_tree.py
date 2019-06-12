91a0a65160b5ea89020cad217864ac9b0d14ba38
aapte123 <apte3000@gmail.com>
2019-06-12 11:11:07 -0700

    Queue Fixed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,41 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # Always wrap value in a node
+        # 1. compare the element against the current node's value
+        # 2. based on the result of comparison, go left or right
+        # 3. If an empty spot is found, park the value there
+        # 4. Otherwise go back to step 1
 
-  def contains(self, target):
-    pass
+        # Base case: Finding an empty spot to park value.
+        if value < self.value:
+            # if value is less, go left
+            # if there is no left child, we park node there
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
 
-  def get_max(self):
-    pass
+        elif value >= self.value:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
-  def for_each(self, cb):
-    pass
+    def contains(self, target):
+        pass
+
+    def get_max(self):
+        pass
+
+    def for_each(self, cb):
+        pass
+
+root = BinarySearchTree(55) # root
+root.insert(90) # root.right
+root.insert(43) # root.left
+root.insert(40) # root.left.left
--- queue/queue.py
+++ queue/queue.py
@@ -1,32 +1,32 @@
 ''' A queue uses FIFO stacking (first in, first out). 
 Therefore, elements are removed from the front and added to the back.'''
 
-class Queue(list):
+class Queue():
     def __init__(self):
         self.size = 0
         # what data structure should we
-        # use to store queue elements? List
-        self.storage = []
+        # use to store queue elements? 
+        self.storage = [] # List
 
     def enqueue(self, item):
-        return self.append(item)
+        return self.storage.append(item)
     
     def dequeue(self):
-        if len(self) == 0:
+        if self.len() == 0:
             return None
-        return self.pop(0) # Default is to pop the last element (aka a stack - LIFO)
+        return self.storage.pop(0) # Default is to pop the last element (aka a stack - LIFO)
         
 
     def len(self):
-        return len(self)
+        return len(self.storage)
 
 
-# q = Queue()
-# q.enqueue(5)
-# q.enqueue(10)
-# q.enqueue(15)
-# q.enqueue(20)
-# print(q)
-# print(q.dequeue())
-# print(q)
-# print(q.len())
+q = Queue()
+q.enqueue(5)
+q.enqueue(10)
+q.enqueue(15)
+q.enqueue(20)
+print(q.storage)
+print(q.dequeue())
+print(q.storage)
+print(q.len())
